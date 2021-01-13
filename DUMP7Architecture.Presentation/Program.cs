1af4eab87ea0283cac101095d9b5eb4f594b75e6
MislavLesin <pripravnik-mislav.lesin@dump.hr>
2021-01-13 19:06:10 +0100

    InitialSetupWtihLayers

---
+++ DUMP7Architecture.Presentation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DUMP7Architecture.Presentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
